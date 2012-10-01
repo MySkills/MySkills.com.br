b27e75fb4168fe74f0453ff821538f52446edaab
Eduardo Cruz <eduardo.cruz@rise.com.br>
2012-09-30 23:08:49 -0300

    Changing mod_rewrite and adding modal window
    Changing .htaccess to remove index.php from url.
    And also ading modal window to sign-up and sign-in buttons.

--- application/views/pages/home.blade.php
+++ application/views/pages/home.blade.php
@@ -8,15 +8,28 @@
 				    <h1>Meet the best developers.</h1>
 				    <h2>Hire great professionals.</h2>
 					<p class="landing-actions">
-						<a href="javascript:;" class="btn btn-large btn-info">Sign-Up!</a>
-						<a href="javascript:;" target="_blank" class="btn btn-large">Sign-In</a>
+						<a href="#myModal" class="btn btn-large btn-info" data-toggle="modal">Sign-Up!</a>
+						<a href="#myModal" target="_blank" class="btn btn-large" data-toggle="modal">Sign-In</a>
 					</p>
 				</div> <!-- /landing-text -->
 				<div class="span6 landing-screenshot">
-					<iframe width="450" height="321" src="http://www.youtube.com/embed/BCH9lpp1XZg" frameborder="0" allowfullscreen></iframe>
+					<iframe width="450" height="321" src="http://www.youtube.com/embed/nGh4Q01cwnk" frameborder="0" allowfullscreen></iframe>
 				</div>                
             </div> <!-- .row -->
         </div> <!-- /container -->
     </div> <!-- /inner -->
-</div> <!-- /landing --> 
+</div> <!-- /landing -->
+<!-- Modal -->
+<div class="modal hide" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  	<div class="modal-header">
+    	<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+    	<h3 id="myModalLabel">Thank you</h3>
+  	</div>
+  	<div class="modal-body">
+    	<p>We are very glad that you are interested. We will deploy this feature really soon...</p>
+  	</div>
+  	<div class="modal-footer">
+    	<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+  	</div>
+</div>
 @endsection
