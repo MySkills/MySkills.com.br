08df9ae288f55c75569ae545b22dab3d557d9ee2
Eduardo Cruz <eduardo.cruz@rise.com.br>
2012-10-01 23:37:41 -0300

    User Migration

--- application/migrations/2012_10_02_023352_create_users.php
+++ application/migrations/2012_10_02_023352_create_users.php
@@ -10,6 +10,18 @@ class Create_Users {
 	public function up()
 	{
 		//
+		Schema::create('users', function($table) {
+		    $table->increments('id');
+		    $table->string('username', 128);
+		    $table->string('nickname', 128);
+		    $table->string('password', 64);
+		    $table->timestamps();
+		});
+		DB::table('users')->insert(array(
+		    'username'  => 'admin',
+		    'nickname'  => 'Admin',
+		    'password'  => Hash::make('password')
+		));		
 	}
 
 	/**
@@ -20,6 +32,6 @@ class Create_Users {
 	public function down()
 	{
 		//
+		Schema::drop('users');			
 	}
-
 }
