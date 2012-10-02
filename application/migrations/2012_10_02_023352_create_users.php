ff38dee7fdb1b91acd6dcf883786fdbeafb37e7c
Eduardo Cruz <eduardo.cruz@rise.com.br>
2012-10-01 23:35:29 -0300

    Added User migration

---
+++ application/migrations/2012_10_02_023352_create_users.php
@@ -0,0 +1,25 @@
+<?php
+
+class Create_Users {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
