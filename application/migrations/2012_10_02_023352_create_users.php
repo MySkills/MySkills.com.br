e3386ce07ad45687710ff1cfeef4483481fba1eb
Eduardo Cruz <eduardo.cruz@rise.com.br>
2012-10-03 23:26:56 -0300

    Adding email and name to migration

--- application/migrations/2012_10_02_023352_create_users.php
+++ application/migrations/2012_10_02_023352_create_users.php
@@ -13,15 +13,19 @@ class Create_Users {
 		Schema::create('users', function($table) {
 		    $table->increments('id');
 		    $table->string('username', 128);
+		    $table->string('name', 128);		    
 		    $table->string('firstname', 128);
 			$table->string('lastname', 128);
+			$table->string('email', 128);			
 		    $table->string('password', 64);
 		    $table->timestamps();
 		});
 		DB::table('users')->insert(array(
 		    'username'  => 'eduardocruz',
+		    'name'  => 'Eduardo Cruz',		    
 		    'firstname'  => 'Eduardo',
 		    'lastname'  => 'Cruz',		    
+		    'email'  => 'eduardo@eduardocruz.com',		    		    
 		    'password'  => Hash::make('kitty411')
 		));		
 	}
