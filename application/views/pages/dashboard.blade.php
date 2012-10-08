6464598d4fd34f2d57b6c3e987db4c420a4a1a5a
Eduardo Cruz <eduardo.cruz@rise.com.br>
2012-10-08 01:38:22 -0300

    New dashboard content

--- application/views/pages/dashboard.blade.php
+++ application/views/pages/dashboard.blade.php
@@ -1,10 +1,5 @@
 @layout('templates.main')
 @section('content')
-<?php 
-	$facebook = IoC::resolve('facebook-sdk');
-	$uid = $facebook->getUser();								
-?>
-
 <div id="subheader">
 	<div class="inner">
 		<div class="container">
@@ -19,9 +14,7 @@
 					<h3><span class="slash"></span> Welcome to our dashboard</h3>
 					<ol class="faq-list">
 						<li>
-								<h4>Dashboard</h4>
 								<p>Here you can exchange information with other users.</p>	
-								<p>FB-uid -> {{$uid}}</p>
 						</li>
 					</ol>
 			</div> <!-- /span10 -->
