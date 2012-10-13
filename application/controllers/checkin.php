3d1ddfb4e26e28a1a513def6eae5642617c75ad0
Eduardo Cruz <eduardo.cruz@rise.com.br>
2012-10-13 13:28:46 -0300

    Laravel Checkin Controller

---
+++ application/controllers/checkin.php
@@ -0,0 +1,10 @@
+<?php
+
+// File: application/controllers/connect.php
+class Checkin_Controller extends Base_Controller {
+	
+	public function action_laravel()
+	{
+		 return "Daily Check-in: Laravel";
+	}
+}
