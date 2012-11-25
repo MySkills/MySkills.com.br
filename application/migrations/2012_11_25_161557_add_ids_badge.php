0fb7edadc36d19e18eb4a2118dfbc981fc80b1a7
Eduardo Cruz <eduardo.cruz@rise.com.br>
2012-11-25 14:23:57 -0200

    Adding IDS badge

---
+++ application/migrations/2012_11_25_161557_add_ids_badge.php
@@ -0,0 +1,33 @@
+<?php
+
+class Add_Ids_Badge {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+		DB::table('badges')->insert(array(
+		    'name'  => 'IDS',
+		    'description'  => '+10 points!!! You attended a course at IDS.com.br.
+		    Therefore, your earned a new badge.',
+		    'image'  => 'ids-badge.png',
+		    'points'  => '10',
+			'issuer_id'  => '6',
+		));			
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
