2107b749940746df0d0f43b48f40f1ff0517b022
Eduardo Cruz <eduardo.cruz@rise.com.br>
2013-02-03 16:04:23 -0300

    New Users First. Top Users Below.

--- application/views/pages/users.blade.php
+++ application/views/pages/users.blade.php
@@ -7,7 +7,7 @@
 		users U, badges B, badge_user BU
 	WHERE
 		U.id = BU.user_id AND
-	        B.id = BU.badge_id
+		B.id = BU.badge_id
 	GROUP BY
 		U.name
 	order by rank desc");
@@ -26,6 +26,53 @@
 	<div class="container">
 		<div class="row">		
 			<div class="span10">
+
+				<!-- NEW USERS -->
+				<table class="table table-striped table-condensed">
+					<caption>
+						NEW USERS.
+					</caption>
+					<thead>
+						<tr>
+							<th width="10%">Picture</th>
+							<th width="10%">Name</th>
+							<th width="25%">Badges</th>
+							<th width="25%">Skills</th>
+							<th width="10%">Points</th>
+							<th width="20%">Since</th>
+						</tr>
+					</thead>
+					<tbody>
+
+					@foreach ($newusers as $user)
+					<tr>
+						<td>
+							{{HTML::image($user->getImageUrl('square'), $user->name, array('width' => 50, 'height'=>50))}}
+						</td>
+						<td>
+							{{HTML::link('users/'.$user->id, $user->name)}}
+						</td>
+						<td>
+							@foreach ($user->activebadges as $badge)
+								{{HTML::image('img/badges/'.$badge->image, $badge->name, array('width' => 50, 'height'=>50))}}
+							@endforeach
+							@for ($i = 0; $i <= (3-count($user->activebadges)); $i++)
+								{{HTML::image('img/badges/unlock100.png', ' ', array('width' => 50, 'height'=>50))}}
+							@endfor
+						</td>
+						<td>
+							@for ($i = 0; $i <= 3; $i++)
+								{{HTML::image('img/badges/unlock100.png', ' ', array('width' => 50, 'height'=>50))}}
+							@endfor
+						</td>
+						<td>{{$user->getpoints()}}</td>
+						<td>{{$user->created_at}}</td>
+					</tr>
+					@endforeach
+					</tbody>
+				</table>
+
+				<!-- TOP USERS -->
 				<table class="table table-striped table-condensed">
 					<caption>
 						TOP USERS.
@@ -48,22 +95,22 @@
 					?>
 					<tr>
 						<td>
-							{{HTML::image($user->getImageUrl('square'),  $user->name, array('width' => 50, 'height'=>50))}}
+							{{HTML::image($user->getImageUrl('square'), $user->name, array('width' => 50, 'height'=>50))}}
 						</td>
 						<td>
 							{{HTML::link('users/'.$user->id, $user->name)}}
 						</td>
 						<td>
 							@foreach ($user->activebadges as $badge)
-								{{HTML::image('img/badges/'.$badge->image,  $badge->name, array('width' => 50, 'height'=>50))}}
+								{{HTML::image('img/badges/'.$badge->image, $badge->name, array('width' => 50, 'height'=>50))}}
 							@endforeach
 							@for ($i = 0; $i <= (3-count($user->activebadges)); $i++)
-								{{HTML::image('img/badges/unlock100.png',  ' ', array('width' => 50, 'height'=>50))}}
+								{{HTML::image('img/badges/unlock100.png', ' ', array('width' => 50, 'height'=>50))}}
 							@endfor
 						</td>
 						<td>
 							@for ($i = 0; $i <= 3; $i++)
-								{{HTML::image('img/badges/unlock100.png',  ' ', array('width' => 50, 'height'=>50))}}
+								{{HTML::image('img/badges/unlock100.png', ' ', array('width' => 50, 'height'=>50))}}
 							@endfor
 						</td>
 
@@ -73,52 +120,6 @@
 					@endforeach
 					</tbody>
 				</table>
-
-
-				<table class="table table-striped table-condensed">
-					<caption>
-						NEW USERS.
-					</caption>
-					<thead>
-						<tr>
-							<th width="10%">Picture</th>
-							<th width="10%">Name</th>
-							<th width="25%">Badges</th>
-							<th width="25%">Skills</th>
-							<th width="10%">Points</th>
-							<th width="20%">Since</th>
-						</tr>
-					</thead>
-					<tbody>
-
-					@foreach ($newusers as $user)
-					<tr>
-						<td>
-							{{HTML::image($user->getImageUrl('square'),  $user->name, array('width' => 50, 'height'=>50))}}
-						</td>
-						<td>
-							{{HTML::link('users/'.$user->id, $user->name)}}
-						</td>
-						<td>
-							@foreach ($user->activebadges as $badge)
-								{{HTML::image('img/badges/'.$badge->image,  $badge->name, array('width' => 50, 'height'=>50))}}
-							@endforeach
-							@for ($i = 0; $i <= (3-count($user->activebadges)); $i++)
-								{{HTML::image('img/badges/unlock100.png',  ' ', array('width' => 50, 'height'=>50))}}
-							@endfor								
-						</td>
-						<td>
-							@for ($i = 0; $i <= 3; $i++)
-								{{HTML::image('img/badges/unlock100.png',  ' ', array('width' => 50, 'height'=>50))}}
-							@endfor								
-						</td>
-						<td>{{$user->getpoints()}}</td>
-						<td>{{$user->created_at}}</td>
-             		</tr>
-          			@endforeach     
-					</tbody>
-				</table>
-
 
 			</div> <!-- /span8 -->
 			<div class="span2">
@@ -130,5 +131,5 @@
 			</div> <!-- /span4 -->
 		</div> <!-- /row -->
 	</div> <!-- /container -->	
-</div> <!-- /subpage -->   
+</div> <!-- /subpage -->
 @endsection
