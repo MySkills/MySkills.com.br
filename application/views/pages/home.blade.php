2055dabcfc1c40421e64e917c9a1318e04f4704e
Eduardo Cruz <eduardo.cruz@rise.com.br>
2013-02-16 22:20:02 -0300

    Changing background color from white to light gray. And increasing width to go from 4 badges to 5 badges

--- application/views/pages/home.blade.php
+++ application/views/pages/home.blade.php
@@ -1,16 +1,16 @@
 @layout('templates.main')
 @section('content')
-	<div class="row">
+	<div class="row boxback">
 		<div class="span12" id="container">
 			@foreach ($topUsers as $topUser)
 			<?php $user = User::find($topUser->id); ?>
 				<div class="box">
-					{{HTML::image($user->getImageUrl('large'), $user->name, array('width'=>'190', 'class'=>'dev'))}}
+					{{HTML::image($user->getImageUrl('large'), $user->name, array('width'=>'170', 'class'=>'dev'))}}
 					<p>{{HTML::link('/users/'.$user->id, $user->name)}}<p>
-					@foreach ($user->partial_badges(6) as $badge)
+					@foreach ($user->partial_badges(5) as $badge)
 						{{HTML::image('img/badges/'.$badge->image, $badge->name, array('width' => 30, 'height'=>30))}}
 					@endforeach
-					@for ($i = 0; $i <= (5-count($user->activebadges)); $i++)
+					@for ($i = 0; $i <= (4-count($user->activebadges)); $i++)
 						{{HTML::image('img/badges/unlock100.png', ' ', array('width' => 30, 'height'=>30))}}
 					@endfor				
 				</div>
