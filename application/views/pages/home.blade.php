beb2725afd06179f4819e49d4fccb0d061c8d765
Eduardo Cruz <eduardo.cruz@rise.com.br>
2013-02-17 13:49:50 -0300

    Adding new Icons
    Signed-off-by: Eduardo Cruz <eduardo.cruz@rise.com.br>

--- application/views/pages/home.blade.php
+++ application/views/pages/home.blade.php
@@ -35,11 +35,12 @@
 				<div class="box">
 					{{HTML::image($user->getImageUrl('large'), $user->name, array('width'=>'200', 'class'=>'dev'))}}
 					<p>{{HTML::link('/users/'.$user->id, $user->name)}}<p>
+
 					<div class="progress progress-danger">
-					  <div class="bar" style="width: 100%;">30/30</div>
+					  <div class="bar" style="width: 100%;">30/30 <i class="icon-heart"></i></div>
 					</div>
 					<div class="progress progress-info">
-					  <div class="bar" style="width: 100%;">20/20</div>
+					  <div class="bar" style="width: 100%;">20/20 <i class="icon-fire"></i></div>
 					</div>
 
 					@foreach ($user->partial_badges(6) as $badge)
