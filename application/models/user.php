5092114b691ee9144ef91d55cbbe80603c54a6d1
Eduardo Cruz <eduardo.cruz@rise.com.br>
2013-03-19 22:23:53 -0300

    Fixing level query for level 1, 2, and 3.

--- application/models/user.php
+++ application/models/user.php
@@ -163,7 +163,7 @@ class User extends Eloquent
 						left JOIN
 							(
 								SELECT
-								   U.id user_id, U.name, TRUNCATE(count(U.name)/20, 0)+1 level
+								   U.id user_id, U.name, ((count(U.name) > 19 ) + 1)+(count(U.name) > 59)+(count(U.name) > 119) level
 								FROM
 									technologies T,
 									technology_user TU,
@@ -189,7 +189,7 @@ class User extends Eloquent
 						left JOIN
 							(
 								SELECT
-								   U.id user_id, U.name, TRUNCATE(count(U.name)/20, 0)+1 level
+								   U.id user_id, U.name, ((count(U.name) > 19 ) + 1)+(count(U.name) > 59)+(count(U.name) > 119) level
 								FROM
 									technologies T,
 									technology_user TU,
