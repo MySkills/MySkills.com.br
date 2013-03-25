151a4159da8afc99864b8988a0f5492f6c6ad72a
Eduardo Cruz <eduardo.cruz@rise.com.br>
2013-03-25 15:45:24 -0300

    Adding all startup logos to jobs page

--- application/views/manguezal/jobs.blade.php
+++ application/views/manguezal/jobs.blade.php
@@ -10,14 +10,156 @@
 
 <div id="gallery">
 	<div class="item">
-		{{HTML::image('img/manguezal/ubee.png', 'UBee', array('width' => '310', 'height' => '105'))}}
+		<a href="#">{{HTML::image('img/manguezal/eventick.png', 'Eventick', array('width' => '310', 'height' => '105'))}}</a>
 	</div>
 	<div class="item">
-		{{HTML::image('img/manguezal/opara.png', 'Opará', array('width' => '310', 'height' => '105'))}}
+		{{HTML::image('img/manguezal/sequaz.png', 'Sequaz', array('width' => '310', 'height' => '105'))}}
 	</div>
 	<div class="item">
-		{{HTML::image('img/manguezal/ubee.png', 'UBee', array('width' => '310', 'height' => '105'))}}
+		{{HTML::image('img/manguezal/conecta.png', 'Conecta.la', array('width' => '310', 'height' => '105'))}}
 	</div>
+
+	<div class="item">
+		<a href="#">{{HTML::image('img/manguezal/surfguru.png', 'SurfGuru', array('width' => '310', 'height' => '105'))}}</a>
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/drbusca.png', 'Dr. Busca', array('width' => '310', 'height' => '105'))}}
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/eztiva.png', 'eztiva', array('width' => '310', 'height' => '105'))}}
+	</div>
+
+	<div class="item">
+		<a href="#">{{HTML::image('img/manguezal/opara.png', 'Opará', array('width' => '310', 'height' => '105'))}}</a>
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/landscape.png', 'Landscape', array('width' => '310', 'height' => '105'))}}
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/favoritoz.png', 'Favoritoz', array('width' => '310', 'height' => '105'))}}
+	</div>
+
+	<div class="item">
+		<a href="#">{{HTML::image('img/manguezal/redu.png', 'Redu', array('width' => '310', 'height' => '105'))}}</a>
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/bidcorp.png', 'Bidcorp', array('width' => '310', 'height' => '105'))}}
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/mobiclub.png', 'Mobiclub', array('width' => '310', 'height' => '105'))}}
+	</div>
+
+	<div class="item">
+		<a href="#">{{HTML::image('img/manguezal/myskills.png', 'MySkills', array('width' => '310', 'height' => '105'))}}</a>
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/qualitasupply.png', 'Qualita Supply', array('width' => '310', 'height' => '105'))}}
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/pycursos.png', 'PyCursos', array('width' => '310', 'height' => '105'))}}
+	</div>
+
+	<div class="item">
+		<a href="#">{{HTML::image('img/manguezal/ubee.png', 'Ubee', array('width' => '310', 'height' => '105'))}}</a>
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/guava.png', 'Guava', array('width' => '310', 'height' => '105'))}}
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/economize.png', 'Economize', array('width' => '310', 'height' => '105'))}}
+	</div>
+
+	<div class="item">
+		<a href="#">{{HTML::image('img/manguezal/via0800.png', 'Via 0800', array('width' => '310', 'height' => '105'))}}</a>
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/seboslivres.png', 'Sebos Livres', array('width' => '310', 'height' => '105'))}}
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/icaregames.png', 'Icare Games', array('width' => '310', 'height' => '105'))}}
+	</div>
+
+	<div class="item">
+		<a href="#">{{HTML::image('img/manguezal/naips.png', 'Naips', array('width' => '310', 'height' => '105'))}}</a>
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/publie.png', 'Publie', array('width' => '310', 'height' => '105'))}}
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/estuario.png', 'Estuario', array('width' => '310', 'height' => '105'))}}
+	</div>
+
+	<div class="item">
+		<a href="#">{{HTML::image('img/manguezal/likemedia.png', 'LikeMedia', array('width' => '310', 'height' => '105'))}}</a>
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/idealizza.png', 'Idealizza', array('width' => '310', 'height' => '105'))}}
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/lookmobile.png', 'Look Mobile', array('width' => '310', 'height' => '105'))}}
+	</div>
+
+	<div class="item">
+		<a href="#">{{HTML::image('img/manguezal/beagletech.png', 'Beagle Tech', array('width' => '310', 'height' => '105'))}}</a>
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/criticallab.png', 'Critical Lab', array('width' => '310', 'height' => '105'))}}
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/yetidev.png', 'Yeti', array('width' => '310', 'height' => '105'))}}
+	</div>
+
+	<div class="item">
+		<a href="#">{{HTML::image('img/manguezal/pingmind.png', 'Pingmind', array('width' => '310', 'height' => '105'))}}</a>
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/bluwhee.png', 'Bluwhee', array('width' => '310', 'height' => '105'))}}
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/agoracompre.png', 'Agora Compre', array('width' => '310', 'height' => '105'))}}
+	</div>
+
+	<div class="item">
+		<a href="#">{{HTML::image('img/manguezal/say2me.png', 'Say2Me', array('width' => '310', 'height' => '105'))}}</a>
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/mobipass.png', 'Mobi Pass', array('width' => '310', 'height' => '105'))}}
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/Guaraling.png', 'Guaraling', array('width' => '310', 'height' => '105'))}}
+	</div>
+
+	<div class="item">
+		<a href="#">{{HTML::image('img/manguezal/robolivre.png', 'RoboLivre', array('width' => '310', 'height' => '105'))}}</a>
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/azklepio.png', 'Azklepio', array('width' => '310', 'height' => '105'))}}
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/Prodeaf.png', 'Prodeaf', array('width' => '310', 'height' => '105'))}}
+	</div>
+
+	<div class="item">
+		<a href="#">{{HTML::image('img/manguezal/pocketstore.png', 'Pocketstore', array('width' => '310', 'height' => '105'))}}</a>
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/propost.png', 'Pro', array('width' => '310', 'height' => '105'))}}
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/coeus.png', 'Coeus', array('width' => '310', 'height' => '105'))}}
+	</div>
+
+	<div class="item">
+		<a href="#">{{HTML::image('img/manguezal/brechoh.png', 'Brechoh', array('width' => '310', 'height' => '105'))}}</a>
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/plingideas.png', 'Pling Ideas', array('width' => '310', 'height' => '105'))}}
+	</div>
+	<div class="item">
+		{{HTML::image('img/manguezal/30ideas.png', '30 ideas', array('width' => '310', 'height' => '105'))}}
+	</div>
+
+
 </div>
 
 <div id="subpage">
