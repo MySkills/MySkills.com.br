d5f353707960eb3efa002f8de6bfb98d9be44e5a
Eduardo Cruz <eduardo.cruz@rise.com.br>
2013-04-13 10:38:28 -0300

    Adding Eloquent mapping for newest badges on badgetype model.

--- application/models/badgetype.php
+++ application/models/badgetype.php
@@ -1,4 +1,11 @@
 <?php 
 class Badgetype extends Eloquent {
 	
+	public function newbadges()
+	{
+	  return $this->has_many('Badge')->where('active', '=', 1)->order_by('id', 'desc')->get();
+	}
+	
+
+
 }
