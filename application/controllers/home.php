7bdd7a14850b14fd2531e5e29409f2f585fe00d6
Eduardo Cruz <eduardo.cruz@rise.com.br>
2013-04-25 15:55:39 -0300

    Adding system messages to wall

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -34,7 +34,8 @@ class Home_Controller extends Base_Controller {
 	{
 		if (Auth::user()) {
 			$newUsers = User::order_by('created_at', 'desc')->take(10)->get();
-			$wallmessages = DB::table('messages')->where_null('recipient_id')->order_by('created_at', 'desc')->get();
+			//$wallmessages = DB::table('messages')->where_null('recipient_id')->order_by('created_at', 'desc')->get();
+			$wallmessages = Message::wallmessages(40);
 			return View::make('pages.homeuser')
 				->with('wallmessages', $wallmessages)
 				->with('newUsers', $newUsers)
--- application/models/message.php
+++ application/models/message.php
@@ -1,4 +1,32 @@
 <?php 
 class Message extends Eloquent {
-	
+
+	public static function wallmessages($limit)
+	{
+	 return DB::query("SELECT 
+	 	M.id id, M.text text, M.sender_id sender_id, M.created_at created_at 
+	FROM 
+		messages M 
+	where 
+		M.recipient_id is null
+	Union 
+	(select BU.id, concat(U.name, ' conquistou o badge ' , B.name),  1, BU.created_at 
+		from 
+			badge_user BU,
+			badges B,
+			users U
+		where 
+			B.id = BU.badge_id and 
+			U.id = BU.user_id)
+	Union 
+	(select TU.id, concat(U.name, ' fez checkin em ', T.name) TU, 1, TU.created_at 
+	from 
+		technology_user TU,
+		users U,
+		technologies T
+	where
+		T.id = TU.technology_id and
+		U.id = TU.user_id)
+	order by created_at desc LIMIT ".$limit);
+	}	
 }
