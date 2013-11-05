ecab99a78aa5ae90966fac6d512d2e74479aad29
Eduardo Cruz <eduardo.cruz@rise.com.br>
2013-11-04 21:45:31 -0300

    Upgrading twitter bootstrap tag from SPAN to COL-MD

--- application/views/pages/channel.blade.php
+++ application/views/pages/channel.blade.php
@@ -11,14 +11,14 @@
 <div id="subpage">	
 	<div class="container">
 		<div class="row">
-			<div class="span10 pagination-centered">
+			<div class="col-md-10 pagination-centered">
 				<!-- UserVoice JavaScript SDK (only needed once on a page) -->
 				<script>(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/noRybcYKo5PMAEWgH4g2nA.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})()</script>
 
 				<!-- The Classic Widget will be embeded wherever this div is placed -->
 				<div data-uv-inline="classic_widget" data-uv-mode="full" data-uv-primary-color="#cc6d00" data-uv-link-color="#007dbf" data-uv-default-mode="feedback" data-uv-forum-id="171773" data-uv-width="100%" data-uv-height="550px"></div>
 			</div> <!-- /span10 -->
-			<div class="span2">
+			<div class="col-md-2">
 				<div class="sidebar">
 					<h3><span class="slash">{{__('channel.about')}}</span></h3>
 					<p>{{__('channel.about1')}}</p>
